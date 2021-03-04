ff49fe99e53ae661f5cdf538968d4f19ca763e3e
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-03-05 01:25:35 +0530

    implemented : tab layout in DashboardActivity.java
    Signed-off-by: Avinash Vijayvargiya <avinash14022002@gmail.com>

--- app/src/main/java/com/example/abhigrith_authority/LoginActivity.java
+++ app/src/main/java/com/example/abhigrith_authority/LoginActivity.java
@@ -1,11 +1,11 @@
 package com.example.abhigrith_authority;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,11 +15,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn_authority_login_skip= (Button) findViewById(R.id.btn_authority_login_skip);
+        Button btn_authority_login_skip = findViewById(R.id.btn_authority_login_skip);
         btn_authority_login_skip.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(intent);
             }
         });
